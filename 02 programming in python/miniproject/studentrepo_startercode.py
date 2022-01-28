88128ab7474568d54669f4bb7ea760590f39fcd8
mindful-ai <58874523+mindful-ai@users.noreply.github.com>
2022-01-28 09:28:54 +0530

    Add files via upload

---
+++ 02 programming in python/miniproject/studentrepo_startercode.py
@@ -0,0 +1,48 @@
+
+def genReport(dd):
+    pass
+
+def write2file(dd, path):
+    pass
+
+# Read the csv file as a text file
+
+
+
+# ------------------------ csv file is read and its content is available for further processing
+
+# Read the coloums --> name,age,regid,phy,chem,math,bio,avg,rank
+# Read a row --> Vijay,14,HPE001,99,98,97,96,0,0
+# Construct a dictionary out of the column and the row
+# Add that dictionary to the main dictionry that represents the class
+# Repeat this process for all the entries in the csv file
+
+
+
+# ------------------------- csv data is in the form of a dictionary
+
+# Access the dictionary iteratively and calculate the average marks and
+# update in the dictionary
+
+
+
+
+# ------------------------- dictionary updated with average data
+
+# Calculate the rank
+# collect all the averages -> into a list
+# arrange the averages in descending order
+# iterate the entire dictionary and update the rank based on the descending
+# order of averages
+
+
+
+# ------------------------- dictionary updated with rank data
+
+# Re-write the csv file
+
+
+
+# ------------------------- resultant csv file is now ready with averages and ranks updated
+
+genReport(classdict)
